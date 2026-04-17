5cd86faa04631ff6c6ba3069fe7aa73523f81242
adria <adria@REX54X>
2026-04-17 08:08:09 -0600

    Agregar archivos de proyecto.

---
+++ clinica/Program.cs
@@ -0,0 +1,14 @@
+﻿class Cliente
+{
+	private int myVar;
+    private int myVar;
+    private int myVar;
+    private int myVar;
+
+    public int MyProperty
+	{
+		get { return myVar; }
+		set { myVar = value; }
+	}
+
+}
