2bb52c3b2aa74d46369443f5a7788b3f54b43e5e
adria <adria@REX54X>
2026-04-17 08:24:57 -0600

    base

--- clinica/Program.cs
+++ clinica/Program.cs
@@ -1,14 +1,82 @@
 ﻿class Cliente
 {
-	private int myVar;
-    private int myVar;
-    private int myVar;
-    private int myVar;
+	private string nombre;
+    private long dpi;
+    private int telefono;
+    private int edad;
 
-    public int MyProperty
-	{
-		get { return myVar; }
-		set { myVar = value; }
+    public Cliente(string Nombre,long Dpi, int Telefono, int Edad)
+    {
+        this.Nombre = Nombre;
+        this.Dpi = Dpi;
+        this.Telefono = Telefono; 
+        this.Edad = Edad;
+    }
+
+    public string Nombre
+    {
+		get { return nombre; }
+		set 
+        {
+            if (value.Length >= 3)
+            {
+                nombre = value;
+            }
+            else
+            {
+                Console.WriteLine("Nombre no valido!");
+            }
+        }
 	}
 
+    public long Dpi
+
+    {
+        get { return dpi; }
+        set 
+        {
+            if (value.ToString().Length >= 13)
+            {
+                dpi = value;
+            }
+            else
+            {
+                Console.WriteLine("Dpi no valido!");
+            }
+        }
+    }
+
+    public int Telefono
+
+    {
+        get { return telefono; }
+        set 
+        {
+            if (value.ToString().Length == 8)
+            {
+                telefono = value;
+            }
+            else
+            {
+                Console.WriteLine("Telefono no valido!");
+            }
+        }
+    }
+
+    public int Edad
+
+    {
+        get { return edad; }
+        set 
+        {
+            if (value > 0)
+            {
+                edad = value;
+            }
+            else
+            {
+                Console.WriteLine("Edad no valida!");
+            }
+        }
+    }
 }
